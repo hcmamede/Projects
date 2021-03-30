cfa27cfc23c38ccd16c35ce674c9ac5cf68c8965
hcmamede <69489107+hcmamede@users.noreply.github.com>
2021-03-30 14:12:52 -0400

    sql

--- CollegeDataBase/College-db-schema.sql
+++ CollegeDataBase/College-db-schema.sql
@@ -9,6 +9,75 @@ GO
 CREATE SCHEMA College;
 GO
 
+
+CREATE TABLE College.Position
+(
+positionId INT IDENTITY(1,1) PRIMARY KEY,
+positionName NVARCHAR(100) NOT NULL,
+atributes NVARCHAR(500) NOT NULL,
+);
+GO
+
+CREATE TABLE College.Department
+(
+departmentId INT IDENTITY(1,1) PRIMARY KEY,
+departmentName NVARCHAR(200) NOT NULL,
+--headId INT  NOT NULL,
+--FOREIGN KEY (headId) REFERENCES College.Staff(staffId),
+);
+GO
+
+
+
+CREATE TABLE College.Staff
+(
+staffId INT IDENTITY(1,1) PRIMARY KEY,
+firstName NVARCHAR(100) NOT NULL,
+lastName NVARCHAR(100) NOT NULL,
+positionId INT  NOT NULL,
+FOREIGN KEY (positionId) REFERENCES College.Position(positionId),
+departmentId INT  NOT NULL,
+FOREIGN KEY (departmentId) REFERENCES College.Department(departmentId),
+salary NVARCHAR(100) NULL,
+);
+GO
+
+
+CREATE TABLE College.Course
+(
+courseId INT IDENTITY(1,1) PRIMARY KEY,
+courseName NVARCHAR(100) NOT NULL,
+instructorId INT  NOT NULL,
+FOREIGN KEY (instructorId) REFERENCES College.Staff(staffId),
+departmentId INT  NOT NULL,
+FOREIGN KEY (departmentId) REFERENCES College.Department(departmentId),
+);
+GO
+
+
+CREATE TABLE College.Room
+(
+roomId INT IDENTITY(1,1) PRIMARY KEY,
+rName NVARCHAR(200) NOT NULL,
+occupation int not null,
+rDescription NVARCHAR(500) NOT NULL,
+);
+GO
+
+
+CREATE TABLE College.Booking
+(
+bookingId INT IDENTITY(1,1) PRIMARY KEY,
+roomId INT NULL,
+FOREIGN KEY (roomId) REFERENCES College.Room(roomId),
+bDate DATE NOT NULL,
+courseId INT NULL,
+FOREIGN KEY (courseId) REFERENCES College.Course(courseId),
+section NVARCHAR(50) NOT NULL,
+);
+GO
+--DROP TABLE College.Booking;
+--GO
 CREATE TABLE College.Student
 (
 studentId INT IDENTITY(1,1) PRIMARY KEY,
@@ -16,6 +85,7 @@ firstName NVARCHAR(100) NOT NULL,
 lastName NVARCHAR(100) NOT NULL,
 section NVARCHAR(50) NOT NULL,
 courseId INT NULL,
+FOREIGN KEY (courseId) REFERENCES College.Course(courseId),
 feeInfo NVARCHAR(100) NOT NULL,
 phone NVARCHAR(20) NOT NULL,
 email NVARCHAR(100) NOT NULL,
@@ -29,26 +99,18 @@ ALTER TABLE College.Student
 	DEFAULT 'no info' FOR feeInfo;
 GO
 
-CREATE TABLE College.Staff
+
+
+CREATE TABLE College.Section
 (
-staffId INT IDENTITY(1,1) PRIMARY KEY,
-firstName NVARCHAR(100) NOT NULL,
-lastName NVARCHAR(100) NOT NULL,
---position INT  NOT NULL,
---department INT  NOT NULL,
-salary NVARCHAR(100) NOT NULL,
+sectionId INT IDENTITY(1,1) PRIMARY KEY,
+section NVARCHAR(20) NOT NULL,
+sectionInitialTime TIME NOT NULL,
+sectionEndTime TIME NOT NULL,
 );
 GO
 
-
-
-CREATE TABLE College.Course
-(
-courseId INT IDENTITY(1,1) PRIMARY KEY,
-courseName NVARCHAR(100) NOT NULL,
---instructorId INT  NOT NULL,
-);
-GO
+--DROP TABLE College.Section;
 
 SELECT * FROM College.Course;
 GO
@@ -56,92 +118,60 @@ GO
 
 --DROP TABLE College.Course;
 --GO
-CREATE TABLE College.Booking
-(
-bookingId INT IDENTITY(1,1) PRIMARY KEY,
---roomId INT NOT NULL ,
-roomId INT NULL,
-FOREIGN KEY (roomId) REFERENCES College.Room(roomId),
-bDate DATETIME NOT NULL,
-courseId INT NULL,
-FOREIGN KEY (courseId) REFERENCES College.Course(courseId),
-section NVARCHAR(50) NOT NULL,
-);
-GO
+
 
 CREATE NONCLUSTERED INDEX IX_date
 ON College.Booking (bDate);
 GO
 
-CREATE TABLE College.Room
-(
-roomId INT IDENTITY(1,1) PRIMARY KEY,
-rName NVARCHAR(200) NOT NULL,
-occupation int not null,
-rDescription NVARCHAR(500) NOT NULL,
-);
-GO
 
 
 --DROP TABLE College.Booking;
 --GO
 
 
-CREATE NONCLUSTERED INDEX index_date
-ON College.Booking(bdate);
-GO
+--CREATE NONCLUSTERED INDEX index_date
+--ON College.Booking(bdate);
+--GO
 
-CREATE TABLE College.Position
-(
-positionId INT IDENTITY(1,1) PRIMARY KEY,
-pName NVARCHAR(200) NOT NULL,
-atribures NVARCHAR(500) NOT NULL,
-);
-GO
 
-CREATE TABLE College.Department
-(
-departmentId INT IDENTITY(1,1) PRIMARY KEY,
-dName NVARCHAR(200) NOT NULL,
-headId INT  NOT NULL,
-FOREIGN KEY (headId) REFERENCES College.Staff(staffId),
-);
-GO
+
+
 --DROP TABLE College.Departments;
 --GO
 
 
 --FOREIGN KEYS
-ALTER TABLE College.Student
-ADD courseId INT NOT NULL,
-FOREIGN KEY (courseId) REFERENCES College.Course(courseId);
-GO
+--ALTER TABLE College.Student
+--ADD courseId INT NOT NULL,
+--FOREIGN KEY (courseId) REFERENCES College.Course(courseId);
+--GO
 
 
-ALTER TABLE College.Staff
-ADD positionId INT NOT NULL,
-FOREIGN KEY (positionId) REFERENCES College.Position(positionId);
-GO
+--ALTER TABLE College.Staff
+--ADD positionId INT NOT NULL,
+--FOREIGN KEY (positionId) REFERENCES College.Position(positionId);
+--GO
 
-ALTER TABLE College.Staff
-ADD departmentId INT NOT NULL,
-FOREIGN KEY (departmentId) REFERENCES College.Department(departmentId);
-GO
+--ALTER TABLE College.Staff
+--ADD departmentId INT NOT NULL,
+--FOREIGN KEY (departmentId) REFERENCES College.Department(departmentId);
+--GO
 
-ALTER TABLE College.Course
-ADD instructorId INT NOT NULL,
-FOREIGN KEY (instructorId) REFERENCES College.Staff(staffId);
-GO
+--ALTER TABLE College.Course
+--ADD instructorId INT NOT NULL,
+--FOREIGN KEY (instructorId) REFERENCES College.Staff(staffId);
+--GO
 
-ALTER TABLE College.Course
-DROP COLUMN instructorId;
-GO
+--ALTER TABLE College.Course
+--DROP COLUMN instructorId;
+--GO
 
 
-ALTER TABLE College.Booking
-ADD roomId INT NOT NULL,
-FOREIGN KEY (roomId) REFERENCES College.Room(roomId);
-GO
+--ALTER TABLE College.Booking
+--ADD roomId INT NOT NULL,
+--FOREIGN KEY (roomId) REFERENCES College.Room(roomId);
+--GO
 
 ALTER TABLE College.Booking
 ADD  updateAt DATETIME;
@@ -171,12 +201,14 @@ AS
 SELECT * FROM College.Booking WHERE bDate = GETDATE();
 GO
 
-
+SELECT * FROM College.Booking;
 
 
 EXEC  College.SelectTodayBooking;
 GO
 
+SELECT * FROM College.Staff;
+GO
 CREATE TRIGGER College.trg_insert_at
 ON College.Booking
 AFTER INSERT
